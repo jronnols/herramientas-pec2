dbd1f99a51ef051021ddb695d526232f4e7d3938
Jon Erik Ronnols <jronnols@uoc.edu>
2023-04-12 23:54:21 +0200

    Añado dependencia sweetalert2 y javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello world');
+const Swal = require('sweetalert2');
+
+
+const subscribeButton = document.getElementById('suscribir');
+
+subscribeButton.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Suscribir a nuestro boletín de noticias',
+        input: 'email',
+        text: 'Introduce tu dirección de email',
+        icon: 'info',
+        confirmButtonText: '¡Suscríbeme!',
+        cancelButtonText: 'No quiero',
+        showCancelButton: true,
+        showCloseButton: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            //TODO: Guardar email en base de datos
+            console.log('Guardar email en base de datos');
+            Swal.fire(
+                'Hecho!',
+                'Recibirás nuestras noticias: ' + result.value,
+                'success'
+            )
+        }
+    });
+});
